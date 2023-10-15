8a54625f4afc65c889b693ec16fa2fc96a2ab131
Valeria <nelera.gb@gmail.com>
2023-10-15 17:58:51 +0300

    hw_15.10.2023

--- unit-tests-5 (Код семинара)/src/seminars/five/number/MaxNumberModule.java
+++ unit-tests-5 (Код семинара)/src/seminars/five/number/MaxNumberModule.java
@@ -3,5 +3,14 @@ package seminars.five.number;
 import java.util.List;
 
 public class MaxNumberModule {
-
+    public int findMaxNumber(List<Integer> numbers) {
+        if (numbers.isEmpty()) throw new IllegalArgumentException("Collection shouldn't be empty");
+        int maxNumber = numbers.get(0);
+        for (int i = 1; i < numbers.size(); i++) {
+            if (numbers.get(i) > maxNumber) {
+                maxNumber = numbers.get(i);
+            }
+        }
+        return maxNumber;
+    }
 }
